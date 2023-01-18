84b061bbf8039db25af35057a50b68e91a06c10e
Pedro Martins <89556415+martins-pedro@users.noreply.github.com>
2023-01-17 21:43:42 -0300

    condicional if e else

---
+++ 12.js
@@ -0,0 +1,10 @@
+let energia = 10
+let clima = "sol"
+
+if (energia > 70 && clima === "sol") {
+    console.log("Irei a praia")
+} else if (energia > 70) {
+    console.log("Irei ao cinema")
+} else (
+    console.log("Irei dormir")
+)

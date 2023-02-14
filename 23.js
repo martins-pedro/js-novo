35af9eac64bb1ef35936c19ea8eb69e7f6ccbd3e
Pedro Martins <89556415+martins-pedro@users.noreply.github.com>
2023-02-14 07:36:19 -0300

    Arrow Function
    Aprendi que Arrow Function vai me ajudar muito em questão de agilidade e praticidade e que ele faz parte do JavaScript moderno

---
+++ 23.js
@@ -0,0 +1,10 @@
+//Arrow Function - Exemplos
+
+const soma = (v1, v2) => {return v1 + v2}
+console.log(soma(12, 13))
+
+const nome = n => n
+console.log(nome("Pedro"))
+
+const add = v => v+v*2
+console.log(add(2))
